09e665c76f80efc570c31c6848f4d6eb971636cc
Petri Lammi <petri.lammi@tietoteema.fi>
2018-02-22 14:08:47 +0200

    Add missing parameter
    Signed-off-by: Petri Lammi <petri.lammi@tietoteema.fi>

--- manifests/puppetdb.pp
+++ manifests/puppetdb.pp
@@ -15,75 +15,79 @@ class puppetmaster::puppetdb
   String $puppetdb_db_connection_limit,
   String $puppetdb_contrib_package_name,
   Boolean $puppetdb_ssl_deploy_certs,
-  ) {
+  String $puppetdb_postgresql_version,
+)
+{
     
-    if ($puppetdb_database_host == '127.0.0.1') {
-      
-      firewall { '5432 allow incoming database':
-        chain       => 'INPUT',
-        state       => ['NEW'],
-        destination => '127.0.0.1/8',
-        dport       => '5432',
-        proto       => 'tcp',
-        action      => 'accept',
-      }
-    }
-    else {
-      firewall { '5432 allow outgoing database':
-        chain       => 'OUTPUT',
-        state       => ['NEW'],
-        destination => $puppetdb_database_host,
-        dport       => '5432',
-        proto       => 'tcp',
-        action      => 'accept',
-      }                       
-    }
+  if ($puppetdb_database_host == '127.0.0.1') {
     
-    firewall { '8081 allow incoming puppetdb':
+    firewall { '5432 allow incoming database':
       chain       => 'INPUT',
       state       => ['NEW'],
-      dport       => ['8080','8081'], 
+      destination => '127.0.0.1/8',
+      dport       => '5432',
       proto       => 'tcp',
       action      => 'accept',
-    }           
-    
-    if $puppetdb_manage_dbserver {
-      
-      ::postgresql::server::role { $puppetdb_database_username:
-        password_hash => postgresql_password($puppetdb_database_username, $puppetdb_database_password),
-        connection_limit => $puppetdb_connection_limit,
-      }
-      
-      ::postgresql::server::database_grant { 'Grant access to puppetdb':
-        privilege => 'ALL',
-        db        => $puppetdb_database_name,
-        role      => $puppetdb_database_username,
-      }
-      
-      ::postgresql::server::extension { 'Add trgm':
-        database     => $puppetdb_database_name,
-        package_name => $puppetdb_contrib_package_name,
-        extension    => 'pg_trgm',
-      }    
-    }
-    
-    class { '::puppetdb':
-      listen_address      => $puppetdb_listen_address,
-      listen_port         => $puppetdb_listen_port,
-      ssl_listen_port     => $puppetdb_ssl_listen_port,
-      manage_dbserver     => $puppetdb_manage_dbserver,
-      database_name       => $puppetdb_database_name,
-      database_host       => $puppetdb_database_host,
-      manage_package_repo => $puppetdb_manage_package_repo,
-      database_username   => $puppetdb_database_username,
-      database_password   => $puppetdb_database_password,
-      ssl_deploy_certs    => $puppetdb_ssl_deploy_certs,
-      require             => Postgresql::Server::Extension['Add trgm'],
-    }
-    
-    class { '::puppetdb::master::config':
-      puppetdb_server     => $puppetdb_puppetdb_server,
-      restart_puppet      => false,
     }
   }
+  else {
+    firewall { '5432 allow outgoing database':
+      chain       => 'OUTPUT',
+      state       => ['NEW'],
+      destination => $puppetdb_database_host,
+      dport       => '5432',
+      proto       => 'tcp',
+      action      => 'accept',
+    }                       
+  }
+  
+  firewall { '8081 allow incoming puppetdb':
+    chain       => 'INPUT',
+    state       => ['NEW'],
+    dport       => ['8080','8081'], 
+    proto       => 'tcp',
+    action      => 'accept',
+  }           
+  
+  class { '::postgresql::globals':
+    manage_package_repo => true,
+    version             => $puppetdb_postgresql_version,
+  }
+  
+  ::postgresql::server::role { $puppetdb_database_username:
+    password_hash => postgresql_password($puppetdb_database_username, $puppetdb_database_password),
+    connection_limit => $puppetdb_connection_limit,
+  }
+  
+  ::postgresql::server::database_grant { 'Grant access to puppetdb':
+    privilege => 'ALL',
+    db        => $puppetdb_database_name,
+    role      => $puppetdb_database_username,
+  }
+  
+  ::postgresql::server::extension { 'Add trgm extension':
+    database     => $puppetdb_database_name,
+    package_name => $puppetdb_contrib_package_name,
+    extension    => 'pg_trgm',
+  }    
+  
+  class { '::puppetdb':
+    listen_address      => $puppetdb_listen_address,
+    listen_port         => $puppetdb_listen_port,
+    ssl_listen_port     => $puppetdb_ssl_listen_port,
+    manage_dbserver     => $puppetdb_manage_dbserver,
+    database_name       => $puppetdb_database_name,
+    database_host       => $puppetdb_database_host,
+    manage_package_repo => $puppetdb_manage_package_repo,
+    database_username   => $puppetdb_database_username,
+    database_password   => $puppetdb_database_password,
+    ssl_deploy_certs    => $puppetdb_ssl_deploy_certs,
+    require             => Postgresql::Server::Extension['Add trgm'],
+  }
+  
+  class { '::puppetdb::master::config':
+    puppetdb_server     => $puppetdb_puppetdb_server,
+    restart_puppet      => true,
+  }
+}
 
