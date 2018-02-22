cdfe3d3ab451311f77047f3bdc3d2efb4cfcf43d
Petri Lammi <petri.lammi@tietoteema.fi>
2018-02-22 23:07:22 +0200

    Fix parameter prefix
    Signed-off-by: Petri Lammi <petri.lammi@tietoteema.fi>

--- manifests/init.pp
+++ manifests/init.pp
@@ -194,7 +194,7 @@ class puppetmaster
   String $foreman_db_database                = 'foreman',
   String $foreman_db_host                    = '127.0.0.1',
   Boolean $foreman_db_manage                 = true,
-  String $db_database                        = 'foreman',
+  String $foreman_db_database                = 'foreman',
   String $foreman_db_password                = 'changeme',
   String $foreman_db_type                    = 'postgresql',
   String $foreman_db_username                = 'foreman',
