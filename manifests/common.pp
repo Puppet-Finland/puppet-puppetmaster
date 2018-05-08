6286a34176e0f1afa152d611198f8ca195c22c5e
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2018-05-08 15:25:05 +0300

    Allow defining allowed IPv4/IPv6 ranges for puppetserver and puppetboard
    This also moves much of what was in common.pp to more proper place in
    puppetserver.pp.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

--- manifests/common.pp
+++ manifests/common.pp
@@ -2,11 +2,8 @@
 #
 class puppetmaster::common
 (
-  Boolean       $manage_packetfilter,
   Array[String] $primary_names,
   String        $timezone,
-  String        $reports_lifetime = '14d',
-  String        $logs_lifetime = '90d',
   Hash          $hosts_entries = {},
 )
 {
@@ -26,71 +23,5 @@ class puppetmaster::common
   class { '::hosts':
     primary_names => $primary_names,
     entries       => $hosts_entries,
-  }
-
-  file { '/var/files':
-    ensure => 'directory',
-    mode   => '0660',
-    owner  => 'puppet',
-    group  => 'puppet',
-  }
-
-  file { '/etc/puppetlabs/puppet/fileserver.conf':
-    ensure => 'present'
-  }
-
-  ini_setting { 'files_path':
-    ensure            => present,
-    path              => '/etc/puppetlabs/puppet/fileserver.conf',
-    section           => 'files',
-    setting           => 'path',
-    value             => '/var/files',
-    key_val_separator => ' ',
-    require           => File['/etc/puppetlabs/puppet/fileserver.conf'],
-  }
-
-  ini_setting { 'files_allow':
-    ensure            => present,
-    path              => '/etc/puppetlabs/puppet/fileserver.conf',
-    section           => 'files',
-    setting           => 'allow',
-    value             => '*',
-    key_val_separator => ' ',
-    require           => File['/etc/puppetlabs/puppet/fileserver.conf'],
-  }
-
-  puppet_authorization::rule { 'files':
-    match_request_path => '^/puppet/v3/file_(content|metadata)s?/files/',
-    match_request_type => 'regex',
-    allow              => '*',
-    sort_order         => 400,
-    path               => '/etc/puppetlabs/puppetserver/conf.d/auth.conf',
-  }
-
-  tidy { '/opt/puppetlabs/server/data/puppetserver/reports':
-    age     => $reports_lifetime,
-    matches => "*.yaml",
-    recurse => true,
-    rmdirs  => false,
-    type    => ctime,
-  }
-
-  tidy { '/var/log/puppetlabs/puppetserver':
-    age     => $logs_lifetime,
-    matches => "puppetserver.*",
-    recurse => true,
-    rmdirs  => false,
-    type    => ctime,
-  }
-
-  if $manage_packetfilter {
-    include ::packetfilter::endpoint
-
-    @firewall { '8140 accept incoming agent traffic to puppetserver':
-      dport  => '8140',
-      proto  => 'tcp',
-      action => 'accept',
-      tag    => 'default',
-    }
   }
 }
--- manifests/puppetboard.pp
+++ manifests/puppetboard.pp
@@ -4,6 +4,10 @@
 #
 # $manage_packetfilter:: Manage IPv4 and IPv6 rules. Defaults to true.
 #
+# $puppetserver_allow_ipv4:: Allow connections to puppetserver from this IPv4 address or subnet. Example: '10.0.0.0/8'. Defaults to '127.0.0.1.
+#
+# $puppetserver_allow_ipv6:: Allow connections to puppetserver from this IPv6 address or subnet. Defaults to '::1'.
+#
 # $server_reports:: Where to store reports. Defaults to 'store,puppetdb'.
 #
 # $autosign:: Set up autosign entries. Set to true to enable naive autosigning.
@@ -17,6 +21,8 @@
 class puppetmaster::puppetboard
 (
   Boolean                  $manage_packetfilter = true,
+  String                   $puppetserver_allow_ipv4 = '127.0.0.1',
+  String                   $puppetserver_allow_ipv6 = '::1',
   String                   $server_reports = 'store,puppetdb',
   Variant[Boolean, String] $autosign = '/etc/puppetlabs/puppet/autosign.conf',
   Optional[Array[String]]  $autosign_entries = undef,
@@ -47,7 +53,6 @@ class puppetmaster::puppetboard
   $puppetdb_ca_cert                       = "${puppetboard_ssl_dir}/ca.pem"
 
   class { '::puppetmaster::common':
-    manage_packetfilter => $manage_packetfilter,
     primary_names       => $primary_names,
     timezone            => $timezone,
     before              => Class['::puppetboard'],
@@ -55,6 +60,8 @@ class puppetmaster::puppetboard
 
   class { '::puppetmaster::puppetdb':
     manage_packetfilter        => $manage_packetfilter,
+    puppetserver_allow_ipv4    => $puppetserver_allow_ipv4,
+    puppetserver_allow_ipv6    => $puppetserver_allow_ipv6,
     server_reports             => $server_reports,
     autosign                   => $autosign,
     autosign_entries           => $autosign_entries,
