4bf05a6a3d759fd4f74b2fc2dd376cf612060bfc
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2018-10-09 14:07:37 +0300

    Separate librarian setup and librarian module install
    There are cases where we want to install librarian but _not_ run librarian
    module install.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

---
+++ vagrant/modules.pp
@@ -0,0 +1,10 @@
+notify { 'Running librarian-puppet': }
+
+exec { 'Run librarian-puppet':
+  cwd       => "${::basedir}",
+  logoutput => true,
+  command   => 'librarian-puppet install --verbose',
+  timeout   => 600,
+  path      => ['/bin','/usr/bin','/opt/puppetlabs/bin','/opt/puppetlabs/puppet/bin'],
+}
+
