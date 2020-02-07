911097ccd3da337f297e72c8e6dcdb3a152ef7d6
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2020-02-07 15:27:22 +0200

    Ensure that r10k always fetches fresh module
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

--- vagrant/modules.pp
+++ vagrant/modules.pp
@@ -3,7 +3,7 @@ notify { 'Running r10k': }
 exec { 'Run r10k':
   cwd       => $::basedir,
   logoutput => true,
-  command   => 'r10k puppetfile install --moduledir=modules',
+  command   => 'r10k puppetfile install --moduledir=modules --force',
   timeout   => 600,
   path      => ['/bin','/usr/bin','/opt/puppetlabs/bin','/opt/puppetlabs/puppet/bin'],
 }
